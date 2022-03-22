84999a3764fb1a73c36df2ddc1f156cfe70eaf4e
Pavel Ciorici <cioricip@gmail.com>
2022-03-22 16:36:25 +0100

    Update wpzoom-portfolio.php

--- wpzoom-portfolio.php
+++ wpzoom-portfolio.php
@@ -14,7 +14,7 @@
  * Author:      WPZOOM
  * Author URI:  https://www.wpzoom.com
  * Text Domain: wpzoom-portfolio
- * Version:     1.0.4
+ * Version:     1.1.0
  * License:     GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
@@ -23,7 +23,7 @@
 defined( 'ABSPATH' ) || exit;
 
 if ( ! defined( 'WPZOOM_PORTFOLIO_VERSION' ) ) {
-	define( 'WPZOOM_PORTFOLIO_VERSION', '1.0.4' );
+	define( 'WPZOOM_PORTFOLIO_VERSION', '1.1.0' );
 }
 
 define( 'WPZOOM_PORTFOLIO__FILE__', __FILE__ );
@@ -57,7 +57,7 @@ class WPZOOM_Blocks {
 	 * @access public
 	 * @since  1.0.0
 	 */
-	public const VERSION = '1.0.4';
+	public const VERSION = '1.1.0';
 
 	/**
 	 * Whether the plugin has been initialized.
