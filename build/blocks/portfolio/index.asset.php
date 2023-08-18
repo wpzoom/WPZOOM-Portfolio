fcd8c8546912ef734f6004dd7f5813ab28a32708
Alexei Samarschi <samarschi@gmail.com>
2023-08-18 14:40:30 +0300

    update build

--- build/blocks/portfolio/index.asset.php
+++ build/blocks/portfolio/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render'), 'version' => '114d2d4561a03dd428cdadea234c43ce');
+<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render'), 'version' => '13ae05ea7ba54643a6736e364212bbb9');
